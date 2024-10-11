79e30b75f2c27d346dd2c021835b6f3aca166c65
Leonardo Correia <135469522+LeonardoTCorreia@users.noreply.github.com>
2024-10-10 21:08:24 -0300

    Update

--- hello.py
+++ hello.py
@@ -1,42 +1,21 @@
-# A very simple Flask Hello World app for you to get started with...
-from flask import Flask, request
+from datetime import datetime
+from flask importe Flask, render_template
+from flask bootstrap import bootstrap
+from flask_moment import moment
 
 app = Flask(__name__)
 
+bootstrap = bootstrap(app)
+moment = moment(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)    
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
-def index():
-    return '''
-        <h1>Avaliação contínua: Aula 030</h1>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/user/Leonardo Correia/PT3026621/IFSP">Identificação</a></li>
-            <li><a href="/contexto_requisicao">Contexto da requisição</a></li>
-        </ul>
-    '''
-
-
-@app.route('/user/<name>/<prontuario>/<instituicao>')
-def user(name, prontuario, instituicao):
-    return f'''
-        <h1>Avaliação contínua: Aula 030</h1>
-        <h2>Aluno: {name}</h2>
-        <h2>Prontuário: {prontuario}</h2>
-        <h2>Instituição: {instituicao}</h2>
-        <p><a href="../../../">Voltar</a></p>
-    '''
-
-# Rota para Contexto da Requisição
-@app.route('/contexto_requisicao')
-def contexto_requisicao():
-    user_agent = request.headers.get('User-Agent')  # Navegador do cliente
-    remote_ip = request.remote_addr  # IP do cliente
-    host = request.host  # Host da aplicação
-
-    return f'''
-    <h1>Contexto da Requisição</h1>
-    <p>Seu navegador é: {user_agent}</p>
-    <p>O IP do computador remoto é: {remote_ip}</p>
-    <p>O host da aplicação é: {host}</p>
-    <a href="/">Voltar</a>
-    '''
+    def index():
+        return render_template("index.html", current)
